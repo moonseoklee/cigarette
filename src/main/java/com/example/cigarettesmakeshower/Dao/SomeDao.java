33bd067f98a412edee8d049c55dbb44ef79dd8e6
winnyiee@naver.com <winnyiee@naver.com>
2020-07-08 12:48:11 +0900

    mysql to h2 db

--- src/main/java/com/example/cigarettesmakeshower/Dao/SomeDao.java
+++ src/main/java/com/example/cigarettesmakeshower/Dao/SomeDao.java
@@ -2,6 +2,8 @@ package com.example.cigarettesmakeshower.Dao;
 
 import com.example.cigarettesmakeshower.Model.SomeModel;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
